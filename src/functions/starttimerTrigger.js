e7191297ff32e940f2e1f6999e5a5666d4f3bf21
Sima21 <af.akhtari@yahoo.de>
2024-12-12 11:53:52 +0100

    Änderungen für die Zeitplanung in stoptimerTrigger.js und starttimerTrigger.js vorgenommen

--- src/functions/starttimerTrigger.js
+++ src/functions/starttimerTrigger.js
@@ -2,18 +2,17 @@ const { app } = require('@azure/functions');
 const { ComputeManagementClient } = require('@azure/arm-compute');
 const { DefaultAzureCredential } = require('@azure/identity');
 
-// Definiere die Timer-Trigger Funktion für das Starten der VM
 app.timer('starttimerTrigger', {
-    schedule: '0 */1 * * *', // Täglich um 8:00 Uhr
+    schedule: '0 8 * * *', 
     handler: async (myTimer, context) => {
         context.log('VM Start Timer Trigger ausgeführt.');
         
-        // Azure Subscription ID und Ressourcengruppe/VM-Daten
+        
         const subscriptionId = process.env['afsar.akhtari@docc.techstarter.de'];
         const resourceGroupName = 'rg-24-04-on-akhtari-afsar';
         const vmName = 'VM-Maintenance-Automation1';
 
-        // Authentifizierung mit DefaultAzureCredential
+        
         const credentials = new DefaultAzureCredential();
         const computeClient = new ComputeManagementClient(credentials, subscriptionId);
 
--- src/functions/stoptimerTrigger.js
+++ src/functions/stoptimerTrigger.js
@@ -2,18 +2,18 @@ const { app } = require('@azure/functions');
 const { ComputeManagementClient } = require('@azure/arm-compute');
 const { DefaultAzureCredential } = require('@azure/identity');
 
-// Definiere die Timer-Trigger Funktion für das Stoppen der VM
+
 app.timer('stoptimerTrigger', {
     schedule: '0 0 18 * * *', 
     handler: async (myTimer, context) => {
         context.log('VM Stop Timer Trigger ausgeführt.');
         
-        // Azure Subscription ID und Ressourcengruppe/VM-Daten
+        
         const subscriptionId = process.env['afsar.akhtari@docc.techstarter.de'];
         const resourceGroupName = 'rg-24-04-on-akhtari-afsar';
         const vmName = 'VM-Maintenance-Automation1';
 
-        // Authentifizierung mit DefaultAzureCredential
+        
         const credentials = new DefaultAzureCredential();
         const computeClient = new ComputeManagementClient(credentials, subscriptionId);
 
